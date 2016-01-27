5a222b3747df974a6d36df657538ca68bd7b3e8c
FACOMP <FACOMP@LAB1-PC16>
2016-01-27 16:34:23 -0300

    Classe Mago

---
+++ Mago.h
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+class Mago{
+	
+	public:
+	    Mago();
+    	bool atacar();
+    	bool defender();
+	    bool pegarItem();
+	    
+	private:
+        int forca = 0;
+	    int defesa = 162;
+	    int magia = 300;
+	    int defesaEspecial = 255;
+	    int velocidade = 228;
+};
+
