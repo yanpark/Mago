e4a3085ba7dac18380a710789fa2b449652716ce
yanpark <yankarlos14@hotmail.com>
2016-01-27 16:35:53 -0300

    Update Mago.h

--- Mago.h
+++ Mago.h
@@ -5,16 +5,17 @@ using namespace std;
 class Mago{
 	
 	public:
-	    Mago();
-    	bool atacar();
-    	bool defender();
-	    bool pegarItem();
+		Mago();
+    		bool atacar();
+    		bool defender();
+	    	bool pegarItem();
 	    
 	private:
-        int forca = 0;
-	    int defesa = 162;
-	    int magia = 300;
-	    int defesaEspecial = 255;
-	    int velocidade = 228;
+		string nome;
+        	int forca = 0;
+	    	int defesa = 162;
+	    	int magia = 300;
+	    	int defesaEspecial = 255;
+	    	int velocidade = 228;
 };
 
