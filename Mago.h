0157c785088ae3a354c382296a594920dd44edc3
yanpark <yankarlos14@hotmail.com>
2016-01-31 00:55:47 -0300

    Update Mago.h

--- Mago.h
+++ Mago.h
@@ -10,33 +10,17 @@ using std::string;
 class Mago{
 	
 	public:
-	    Mago();
+		Mago();
 		
 		void verificarStatus(); // Verifica os status do Mago
 		void batalhar();
 		bool atacar(); 
-<<<<<<< HEAD
-    	bool defender();
-    	bool verificarItem(bool item); // Verifica se há um item no chão
-=======
     		bool defender();
     		bool verificarItem(bool item); // Verifica se hÃ¡ um item no chÃ£o
-    		bool defender();
->>>>>>> branch 'master' of https://github.com/yanpark/Mago.git
     	
 	private:
 		string nome;
-<<<<<<< HEAD
-        int forca;
-	    int defesa;
-	    int magia;
-	    int defesaEspecial;
-	    int velocidade;
-	    double mana, hp;
-	    bool sucesso, item;
-	    int opcao;
-=======
-        	int forca;
+		int forca;
 	    	int defesa;
 	    	int magia;
 	    	int defesaEspecial;
@@ -44,7 +28,6 @@ class Mago{
 	    	double mana, hp;
 	    	bool sucesso, item;
 	    	int opcao;
->>>>>>> branch 'master' of https://github.com/yanpark/Mago.git
 };
 
 #endif // MAGO_H
