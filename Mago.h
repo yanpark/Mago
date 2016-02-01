97591ee8f4898151dd99598da7f0362bb9db1ea8
yanpark <yankarlos14@hotmail.com>
2016-02-01 16:46:21 -0300

    Update Mago.h

--- Mago.h
+++ Mago.h
@@ -15,19 +15,19 @@ class Mago{
 		void verificarStatus(); // Verifica os status do Mago
 		void batalhar();
 		bool atacar(); 
-    	bool defender();
-    	bool verificarItem(bool); // Verifica se hÃ¡ um item no chÃ£o
+		bool defender();
+		bool verificarItem(bool); // Verifica se hÃ¡ um item no chÃ£o
     	
 	private:
 		string nome;
-        int forca;
-	    int defesa; // resistência a ataques físicos
-	    int magia;
-	    int defesaEspecial; // resistência a ataques mágicos
-	    int velocidade;
-	    double mana, hp;
-	    bool sucesso, item;
-	    int opcao, level;
+		int forca;
+		int defesa; // resistência a ataques físicos
+		int magia;
+		int defesaEspecial; // resistência a ataques mágicos
+		int velocidade;
+		double mana, hp;
+		bool sucesso, item;
+		int opcao, level;
 };
 
 #endif // MAGO_H
