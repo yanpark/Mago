0b1b94573265272c9c5ffa6275874adaa0ec8c17
yanpark <yankarlos14@hotmail.com>
2016-02-02 00:13:41 -0300

    Update Mago.h

--- Mago.h
+++ Mago.h
@@ -15,49 +15,17 @@ class Mago{
 		void verificarStatus(); // Verifica os status do Mago
 		void batalhar();
 		bool atacar(); 
-<<<<<<< HEAD
-    	bool defender();
-    	bool verificarItem(bool); // Verifica se há um item no chão
-=======
-<<<<<<< HEAD
-    	bool defender();
-    	bool verificarItem(bool); // Verifica se há um item no chão
-=======
 		bool defender();
 		bool verificarItem(bool); // Verifica se hÃ¡ um item no chÃ£o
->>>>>>> branch 'master' of https://github.com/yanpark/Mago.git
->>>>>>> branch 'master' of https://github.com/yanpark/Mago.git
     	
 	private:
 		string nome;
-<<<<<<< HEAD
-        int forca;
-	    int defesa; // resist�ncia a ataques f�sicos
-	    int magia;
-	    int defesaEspecial; // resist�ncia a ataques m�gicos
-	    int mana, hp, dano, opcao, level;
-	    bool sucesso, item;
-=======
-<<<<<<< HEAD
-        int forca;
-	    int defesa; // resist�ncia a ataques f�sicos
-	    int magia;
-	    int defesaEspecial; // resist�ncia a ataques m�gicos
-	    int velocidade;
-	    double mana, hp;
-	    bool sucesso, item;
-	    int opcao, level;
-=======
 		int forca;
-		int defesa; // resistência a ataques físicos
-		int magia;
-		int defesaEspecial; // resistência a ataques mágicos
-		int velocidade;
-		double mana, hp;
-		bool sucesso, item;
-		int opcao, level;
->>>>>>> branch 'master' of https://github.com/yanpark/Mago.git
->>>>>>> branch 'master' of https://github.com/yanpark/Mago.git
+	   	int defesa; // resistência a ataques físicos
+	    	int magia;
+	    	int defesaEspecial; // resistência a ataques mágicos
+	    	int mana, hp, dano, opcao, level;
+	    	bool sucesso, item;
 };
 
 typedef struct{
