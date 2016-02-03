2d4bbed8f49b9f07c855a276ec1167da466f7ffc
yanpark <yankarlos14@hotmail.com>
2016-02-03 01:02:05 -0300

    Update Mago.h

--- Mago.h
+++ Mago.h
@@ -12,16 +12,16 @@ class Mago{
 	public:
 	    Mago(const string &, int);
 		
-		void verificarStatus(); // Verifica os status do Mago
+		void verificarStatus(); // Verifica os status de batalha do Mago
 		void batalhar();
 		bool atacar(); 
 		bool defender();
-		bool verificarItem(bool); // Verifica se hÃ¡ um item no chÃ£o
+		bool verificarItem(bool); // Verifica se há um item no chão
     	
 	private:
 		string nome;
 		int forca;
-	   	int defesa; // resistência a ataques físicos
+	    	int defesa; // resistência a ataques físicos
 	    	int magia;
 	    	int defesaEspecial; // resistência a ataques mágicos
 	    	int mana, hp, dano, opcao, level;
@@ -29,9 +29,8 @@ class Mago{
 };
 
 typedef struct{
-	int hp_ini = 100;
-	int forca_ini = 82;
-	int defesa_ini = 120;
-	int def_esp_ini = 78;	
+	int hp_ini = 125;
+	int forca_ini = 98 ;
+	int def_esp_ini = 95;	
 } Inimigo;
 #endif // MAGO_H
