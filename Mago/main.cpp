95c6d8c7d401a1efd5631bed5120e42e3ea2583b
Yan <Yan@SIM-PC>
2016-02-27 23:24:56 -0300

    Atualização 27/02

--- Mago/main.cpp
+++ Mago/main.cpp
@@ -7,6 +7,17 @@ int main(int argc, char **argv){
 	bool opcao; 
 	
 	Mago luz("Gandalf", 1, 5);
+	Mago trevas("Gandalf", 1, 5);
+	
+	cout << "Mago de luz: " << luz;
+	cout << "Mago de trevas: " << trevas;
+	
+	if(luz == trevas){
+		cout << "Os magos sao iguais\n" << endl;
+	}
+	else{
+		cout << "Os magos nao sao iguais\n" << endl;
+	}
 	
 	Mago::verificarStatus(luz);
 	system("pause");
