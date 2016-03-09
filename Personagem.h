71fef5d0af6933a8e37d734867ab7b070a7a6c86
FACOMP <FACOMP@LAB1-PC16>
2016-03-09 16:45:22 -0300

    Atualização herança

--- Personagem.h
+++ Personagem.h
@@ -1,23 +1,22 @@
+#ifndef PERSONAGEM_H
+#define PERSONAGEM_H
+
 #include <iostream>
 #include <string>
 using namespace std;
-
-#ifndef PERSONAGEM_H
-#define PERSONAGEM_H
 
 class Personagem{
 	
     friend ostream &operator<<(ostream &, const Personagem &);
     
     public:
+        
+        Personagem(const string &, int, const int &);
+	    Personagem(const Personagem &);
+	    ~Personagem();
 
-
-		void menuPrincipal();
-		static const void verificarStatus(const Personagem &); 
+        static const void verificarStatus(const Personagem &); 
 		void ganharXP(bool);
-		bool batalhar();
-		bool atacar(); 
-		bool defender();
 		bool verificarItem();
 		void inventario();
 		void imprimirData() const;
@@ -26,7 +25,7 @@ class Personagem{
 		const Personagem &operator=(const Personagem &);
 		bool operator==(const Personagem &) const;
 		
-	private:
+	protected:
 		string nome;
 		int defesa; // resistência a ataques físicos
 	    int defesaEspecial; // resistência a ataques mágico
@@ -39,17 +38,11 @@ class Personagem{
 	    bool xp;
 	    string *ptr_inventario;
 	    string *nomeItens;
-	    int itemInventario = 7;
-	    int cont = 0;
+	    int itemInventario;
+	    int cont;
 	    int quantItens;
 	    int nItens;
 	    int aux;
         static int numeroDePersonagens;
 };
-
-typedef struct{
-	int hp_ini = 125;
-	int forca_ini = 98 ;
-	int def_esp_ini = 95;	
-} Inimigo;
 #endif // MAGO_H
